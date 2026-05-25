1146e5dcfe32b42e04ccbc8d965cb50544ed2968
samitwaru <samuel.itwaru@yukon.ug>
2026-05-25 10:27:56 +0300

    merge translations and tonny chanages

--- src/components/SidebarRight.tsx
+++ src/components/SidebarRight.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useRef, useEffect } from "react";
 import "./SidebarRight.css";
 import type { ThemeIcon, ThemeColors, Mood, ThemeCtaColor } from "../types";
 import { SidebarCtaControls } from "./SidebarCtaControls";
